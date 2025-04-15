7bddaa52df91f7af1daafbc34572ff53609a58d8
RayhanFatihR <rayhanfatihr76@gmail.com>
2025-04-15 15:30:47 +0700

    Struct T2

--- struct.cpp
+++ struct.cpp
@@ -6,3 +6,17 @@ struct mahasiswa{
     string nama;
     string alamat;
 };
+
+int main(){
+    mahasiswa mhs;
+
+    mhs.nim = "20240140097";
+    mhs.nama = "Rayhan";
+    mhs.alamat = "Sonopakis";
+
+    cout << "Mahasiswa NIM =";
+    cin >> mhs.nim;
+    cout << "Mahasiswa Nama =";
+    cin >> mhs.nama;
+    cout << "Mahasiswa Alamat =";
+}
