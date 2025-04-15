0a7b59ee130d65e85c8777646000a15a3303201a
RayhanFatihR <rayhanfatihr76@gmail.com>
2025-04-15 15:01:04 +0700

    first commit

---
+++ array.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+//deklarasikan array tanpa ukuran, tetapi wajib mengisikan data
+int nilai[] = {2,5,9,10,34};
+//deklarasikan array dengan ukuran dan diberikan nilai awal
+int skot[5] = {8, 12, 18, 6};
+//deklarasikan array dengan ukuran dan tindakan ada nilai awal
+string nama[3];
