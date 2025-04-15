f7bb48f13c6d89c60cf2cb5c45a13ff5afda32e2
RayhanFatihR <rayhanfatihr76@gmail.com>
2025-04-15 15:18:28 +0700

    Array T5

--- array.cpp
+++ array.cpp
@@ -4,7 +4,7 @@ using namespace std;
 //deklarasikan array tanpa ukuran, tetapi wajib mengisikan data
 int nilai[] = {2,5,9,10,34};
 //deklarasikan array dengan ukuran dan diberikan nilai awal
-int skot[5] = {8, 12, 18, 6};
+int skor[5] = {8, 12, 18, 6};
 //deklarasikan array dengan ukuran dan tindakan ada nilai awal
 string nama[3];
 
@@ -19,5 +19,8 @@ int main(){
     }
 
     cout << endl;
-    for (int i = 0; i < 3; i++){}
+    for (int i = 0; i < 3; i++){
+        cout << "Data Mahasiswa ke-" << i+1 << "=" << nama[i];
+        << endl;
+    }
 }
