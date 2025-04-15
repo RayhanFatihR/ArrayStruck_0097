d278a97f729119407e9cebcbf64fa221de0fe144
RayhanFatihR <rayhanfatihr76@gmail.com>
2025-04-15 15:54:28 +0700

    Struct3

---
+++ struct3.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+struct detailAlamat{
+    string desa;
+    string kota;
+};
+
+struct mahasiswa{
+    string nim;
+    string nama;
+    detailAlamat alamat;
+};
+
+int main(){
+    mahasiswa mhs;
+
+    cout << "Mahasiswa NIM =";
+    cin >> mhs.nim;
+    cin.ignore();
+    cout << "Mahasiswa Nama =";
+    getline(cin, mhs.nama);
+    cout << "Mahasiswa Alamat Desa =";
+    cin >> mhs.alamat.desa;
+    cout << "Mahasiswa Alamat Kota =";
+    cin >> mhs.alamat.kota;
+
+    cout << endl;
+    cout << "Data Mahasiswa" << endl;
+
+    cout << "NIM =" << mhs.nim << endl;
+    cout << "Nama =" << mhs.nama << endl;
+    cout << "Alamat Desa =" << mhs.alamat.desa << endl;
+    cout << "Alamat Kota =" << mhs.alamat.kota << endl;
+}
